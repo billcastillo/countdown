aa06cde63917b3aefc21ac3b94b68e5e5a3187fc
bill <castillobil@gmail.com>
2024-11-05 03:18:06 +0800

    initial commit

--- src/routes/countdown/+page.ts
+++ src/routes/countdown/+page.ts
@@ -2,7 +2,6 @@ import { decompressFromUrl } from '../../lib/utils/url.js';
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
 
 export function load({ url }) {
 	const iso = url.searchParams.get('iso');
